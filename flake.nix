a09ef890c0407026b3b094f1b6cef720fbf89c13
Johannes Bartschies <johannes.bartschies@gmail.com>
2026-04-12 15:23:16 +0200

    nix package

--- flake.nix
+++ flake.nix
@@ -1,19 +1,74 @@
 {
-  description = "Development environment for deej";
+  description = "Deej - hardware volume mixer for Linux";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = [
-        nixpkgs.legacyPackages.x86_64-linux.go
-        nixpkgs.legacyPackages.x86_64-linux.pkg-config
-        nixpkgs.legacyPackages.x86_64-linux.pulseaudio
-        nixpkgs.legacyPackages.x86_64-linux.playerctl
-        nixpkgs.legacyPackages.x86_64-linux.git
-      ];
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.buildGoModule rec {
+            pname = "deej";
+            version = "1.0.0"; # Using a placeholder version since there's no explicit version
+
+            src = ./.;
+
+            vendorHash = "sha256-9g8AugKTVkT4cucMzcBS/vJk7lukzvS6jKyKMqEe2io=";
+
+            # The main package is actually in pkg/deej/cmd directory
+            subPackages = [ "pkg/deej/cmd" ];
+
+            # Add ldflags to match the build script
+            ldflags = [
+              "-s"
+              "-w"
+              "-X main.versionTag=${version}"
+              "-X main.buildType=release"
+            ];
+
+            # Rename the binary to have a more standard name
+            postInstall = ''
+              mv $out/bin/cmd $out/bin/deej
+            '';
+
+            meta = with pkgs.lib; {
+              description = "Set app volumes with real sliders - a hardware volume mixer for Linux";
+              homepage = "https://github.com/TheScabbage/deej-linux";
+              license = licenses.mit;
+              maintainers = [ ];
+              platforms = platforms.linux;
+            };
+          };
+        });
+
+      overlays.default = final: prev: {
+        deej = self.packages.${prev.system}.default;
+      };
+
+      nixosModules.default = import ./nix/module.nix;
+
+      home-managerModules.default = import ./nix/module.nix;
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.go
+              pkgs.pkg-config
+              pkgs.pulseaudio
+              pkgs.playerctl
+              pkgs.git
+            ];
+          };
+        });
     };
-  };
 }
---
+++ nix/module.nix
@@ -0,0 +1,99 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.services.deej;
+
+  configFile = pkgs.writeText "deej-config.yaml" ''
+    # process names are case-insensitive
+    slider_mapping: ${builtins.toJSON cfg.sliderMapping}
+
+    invert_sliders: ${boolToString cfg.invertSliders}
+
+    com_port: ${cfg.serialPort}
+    baud_rate: ${toString cfg.baudRate}
+
+    noise_reduction: ${toString cfg.noiseReduction}
+  '';
+
+in
+{
+  options.services.deej = {
+    enable = mkEnableOption "deej volume control service";
+
+    sliderMapping = mkOption {
+      type = types.attrsOf (types.either types.str (types.listOf types.str));
+      default = {
+        "0" = "master";
+        "1" = [ "firefox" ];
+        "2" = "deej.unmapped";
+        "3" = [ "Chromium" "electron" ];
+        "4" = [ "WEBRTC VoiceEngine" ".Discord-wrapped" ];
+        "5" = "mic";
+      };
+      description = "Mapping of slider numbers to applications or 'master'/'mic'";
+    };
+
+    invertSliders = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Whether to invert slider values";
+    };
+
+    serialPort = mkOption {
+      type = types.str;
+      default = "/dev/ttyUSB0";
+      description = "Serial port for the deej device";
+    };
+
+    baudRate = mkOption {
+      type = types.int;
+      default = 9600;
+      description = "Baud rate for serial communication";
+    };
+
+    noiseReduction = mkOption {
+      type = types.float;
+      default = 0.025;
+      description = "Noise reduction value";
+    };
+
+    verbose = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable verbose logging";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Install the binary
+    home.packages = [ pkgs.deej ];
+
+    # Create config file
+    home.file.".config/deej/config.yaml".source = configFile;
+
+    # Systemd user service
+    systemd.user.services.deej = {
+      Unit = {
+        Description = "Deej volume control";
+        After = [ "graphical-session.target" ];
+        Wants = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        ExecStart = "${pkgs.deej}/bin/deej${optionalString cfg.verbose " -v"}";
+        Restart = "always";
+        RestartSec = 5;
+        WorkingDirectory = "%h/.config/deej";
+      };
+
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+
+    # Enable user services
+    systemd.user.startServices = "sd-switch";
+  };
+}
